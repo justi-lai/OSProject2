87c28f622963a338fc989a13ef032b03f4402b13
justi <justin88.lai@gmail.com>
2024-10-21 19:51:23 -0500

    Added in a few methods

--- Bank.java
+++ Bank.java
@@ -1,5 +1,21 @@
+import java.util.concurrent.Semaphore;
+
 public class Bank {
     public static void main(String[] args) {
         
+    }
+}
+
+class Teller extends Thread {
+    @Override
+    public void run() {
+
+    }
+}
+
+class Customer extends Thread{
+    @Override
+    public void run() {
+
     }
 }
